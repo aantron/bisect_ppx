9adffae786435f447824cccfaaff4387889e4328
Anton Bachin <antonbachin@yahoo.com>
2016-02-10 08:39:20 -0600

    Fix gh-pages postprocessor
    - We wrapped the GIF in a link a while ago. OMD doesn't handle this
      well, so added a fix-up to the postprocessor.
    - Current Lambda Soup parses entities, unlike before, so adjusted how
      we detect unconverted <br> tags.

--- doc/postprocess.ml
+++ doc/postprocess.ml
@@ -11,9 +11,20 @@ let () =
   (* OMD messes up the badge links in the header. Replace it. *)
   create_element ~inner_text:"Bisect_ppx" "h1" |> replace (soup $ "h1");
 
+  (* OMD doesn't handle an image wrapped in an anchor well. Fix the main
+     image. *)
+  let gif = soup $ "p img[src]" in
+  let container = R.parent gif in
+  let link = container $ "a" in
+
+  clear container;
+  clear link;
+  append_child container link;
+  append_child link gif;
+
   (* OMD seems to interpret <br> tags as text, and turns them into
      paragraphs. Fix that. *)
-  soup $$ "p:contains(\"&lt;br\")"
+  soup $$ "p:contains(\"<br\")"
   |> iter (fun e -> create_element "br" |> replace e);
 
   (* Write the modified HTML to STDOUT. *)
