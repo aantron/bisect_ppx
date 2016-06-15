7846f552d3d90fdeca69fdc7fffc41aaae6ecb9a
Anton Bachin <antonbachin@yahoo.com>
2016-06-15 14:18:59 -0500

    Adjust whitespace
    Consequence of preceding commit.

--- src/report/reportGeneric.ml
+++ src/report/reportGeneric.ml
@@ -29,34 +29,34 @@ class type converter =
 
 let output_file verbose in_file conv visited points =
   verbose (Printf.sprintf "Processing file '%s'..." in_file);
-    let cmp_content = Hashtbl.find points in_file |> Common.read_points' in
-    verbose (Printf.sprintf "... file has the following points: %s"
-      (String.concat ","
-        (List.map (fun pd ->
-          Printf.sprintf "[%d %d]\n" pd.Common.offset pd.Common.identifier)
-          cmp_content)));
-    let len = Array.length visited in
-    let stats = ReportStat.make () in
-    let points =
-      List.map
-        (fun p ->
-          let nb =
-            if p.Common.identifier < len then
-              visited.(p.Common.identifier)
-            else
-              0 in
-          ReportStat.update stats (nb > 0);
-          (p.Common.offset, nb))
-        cmp_content in
-    let buffer = Buffer.create 64 in
-    Buffer.add_string buffer (conv#file_header in_file);
-    Buffer.add_string buffer (conv#file_summary stats);
-    List.iter
-      (fun (ofs, nb) ->
-        Buffer.add_string buffer (conv#point ofs nb))
-      points;
-    Buffer.add_string buffer (conv#file_footer in_file);
-    Some (Buffer.contents buffer, stats)
+  let cmp_content = Hashtbl.find points in_file |> Common.read_points' in
+  verbose (Printf.sprintf "... file has the following points: %s"
+    (String.concat ","
+      (List.map (fun pd ->
+        Printf.sprintf "[%d %d]\n" pd.Common.offset pd.Common.identifier)
+        cmp_content)));
+  let len = Array.length visited in
+  let stats = ReportStat.make () in
+  let points =
+    List.map
+      (fun p ->
+        let nb =
+          if p.Common.identifier < len then
+            visited.(p.Common.identifier)
+          else
+            0 in
+        ReportStat.update stats (nb > 0);
+        (p.Common.offset, nb))
+      cmp_content in
+  let buffer = Buffer.create 64 in
+  Buffer.add_string buffer (conv#file_header in_file);
+  Buffer.add_string buffer (conv#file_summary stats);
+  List.iter
+    (fun (ofs, nb) ->
+      Buffer.add_string buffer (conv#point ofs nb))
+    points;
+  Buffer.add_string buffer (conv#file_footer in_file);
+  Some (Buffer.contents buffer, stats)
 
 let output verbose file conv data points =
   let files, stats = Hashtbl.fold
