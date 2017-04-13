63c6a13ec95b33adcb545bd3787e0502eeb139e5
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-04-13 18:36:45 -0400

    Let omp handle the arg specs

--- src/syntax/ppx_bisect.ml
+++ src/syntax/ppx_bisect.ml
@@ -4,10 +4,4 @@ open Ppx_tools_404
 let () =
   Driver.register ~name:"ppx_bisect" ~args:InstrumentArgs.switches
     Versions.ocaml_404 (fun _config _cookies ->
-        (* let anon s = raise (invalid_arg ("nothing anonymous: " ^ s)) in *)
-        let anon _s = () in
-        let usage = Printf.sprintf "Usage: bisect_ppx <options>" in
-        let arga = Array.of_list ("" :: (Array.to_list Sys.argv)) in
-        Arg.parse_argv arga InstrumentArgs.switches anon usage;
-        Ast_mapper_class.to_mapper (new InstrumentPpx.instrumenter)
-      )
+        Ast_mapper_class.to_mapper (new InstrumentPpx.instrumenter))
