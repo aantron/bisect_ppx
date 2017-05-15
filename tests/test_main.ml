bbbe41bef84b601746dbda1e5a876de9e9850722
Gabriel Scherer <gabriel.scherer@gmail.com>
2017-05-14 20:07:21 -0400

    test for IGNORE/VISIT comments in patterns

--- tests/test_main.ml
+++ tests/test_main.ml
@@ -28,6 +28,7 @@ let tests = "bisect_ppx" >::: [
   Test_comments.tests;
   Test_exclude.tests;
   Test_exclude_file.tests;
+  Test_exclude_comments.tests;
   Test_ppx_integration.tests;
   Test_thread_safety.tests;
   Test_ounit_integration.tests;
