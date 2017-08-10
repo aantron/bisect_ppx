75057809b26e8b7c514e13f126f1444d7f21af28
Anton Bachin <antonbachin@yahoo.com>
2017-08-10 16:10:15 -0500

    Support conditional instrumentation
    This is a workaround for Jbuilder. Jbuilder lacks a good way to
    conditionally disable PPXs.
    
    When the option -conditional is passed to Bisect_ppx during
    preprocessing, it checks for the BISECT_ENABLE environment variable. If
    it is present and set to YES (case-insensitive), Bisect_ppx instruments
    code as normal. If it is absent, or not set to YES, Bisect_ppx does not
    modify the AST.
    
    When -conditional is not passed, Bisect_ppx instruments as normal. This
    behavior is backwards-compatible.
    
    For typical usage, see the new usage test in
    
      test/usage/jbuilder-conditional
    
    However, the -simple-cases option is just an example, and should not be
    used for any production code.

--- src/syntax/ppx_bisect.ml
+++ src/syntax/ppx_bisect.ml
@@ -3,5 +3,24 @@ open Ppx_tools_404
 
 let () =
   Driver.register ~name:"bisect_ppx" ~args:InstrumentArgs.switches
-    Versions.ocaml_404 (fun _config _cookies ->
-        Ast_mapper_class.to_mapper (new InstrumentPpx.instrumenter))
+    Versions.ocaml_404 begin fun _config _cookies ->
+      let enabled =
+        match !InstrumentArgs.conditional with
+        | false ->
+          `Enabled
+        | true ->
+          match Sys.getenv "BISECT_ENABLE" with
+          | exception Not_found ->
+            `Disabled
+          | s when (String.uppercase [@ocaml.warning "-3"]) s = "YES" ->
+            `Enabled
+          | _ ->
+            `Disabled
+      in
+
+      match enabled with
+      | `Enabled ->
+        Ast_mapper_class.to_mapper (new InstrumentPpx.instrumenter)
+      | `Disabled ->
+        Ast_404.Ast_mapper.default_mapper
+    end
