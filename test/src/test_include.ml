fb1767a3c8ddbfd4f0bf33ee5b9369f724d4aab5
Anton Bachin <antonbachin@yahoo.com>
2017-10-05 09:32:22 -0500

    [SQUASH] More thorough module include test

--- test/src/test_include.ml
+++ test/src/test_include.ml
@@ -8,10 +8,12 @@ open OUnit2
 open Test_helpers
 
 (* If including A in b.ml causes a duplicate module name error, compilation of
-   b.ml will fail. *)
+   b.ml will fail. If including A in b.ml shadows ___bisect_visit___, running
+   ./a.out will fail. *)
 let tests = "include" >::: [
   test "include" begin fun () ->
     compile ((with_bisect ()) ^ " -c") "fixtures/include/a.ml";
-    compile ((with_bisect ()) ^ " a.cm[ox]") "fixtures/include/b.ml"
+    compile ((with_bisect ()) ^ " a.cm[ox]") "fixtures/include/b.ml";
+    run "./a.out"
   end;
 ]
