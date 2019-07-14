e5d430b8a883b208629265f21c9af60868a44c16
Anton Bachin <antonbachin@yahoo.com>
2019-07-14 15:23:53 +0300

    Test interaction of [@@@coverage.off] and include

---
+++ test/unit/fixtures/attributes/include.reference.ml
@@ -0,0 +1,19 @@
+module Bisect_visit___include___ml =
+  struct
+    let ___bisect_visit___ =
+      let point_definitions =
+        "\132\149\166\190\000\000\000\004\000\000\000\002\000\000\000\005\000\000\000\005\144\160i@" in
+      let `Staged cb =
+        Bisect.Runtime.register_file "include.ml" ~point_count:1
+          ~point_definitions in
+      cb
+  end
+open Bisect_visit___include___ml
+module Foo =
+  struct
+    let instrumented = ___bisect_visit___ 0; ()
+    [@@@coverage.off ]
+    let not_instrumented = ()
+  end
+[@@@coverage.off ]
+include Foo
