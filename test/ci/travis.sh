c462a26793a35f9f5e33564b693940054117d361
Anton Bachin <antonbachin@yahoo.com>
2019-08-27 13:38:20 +0300

    Travis: clear cache in cron jobs

--- test/ci/travis.sh
+++ test/ci/travis.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
+if [ "$TRAVIS_EVENT_TYPE" == cron ]
+then
+    rm -rf ~/.opam
+    rm -rf ./_opam
+    rm -rf ~/.esy
+    rm -rf ./test/bucklescript/node_modules
+    rm -rf ./node_modules
+    rm -rf ./_cache
+fi
+
 if [ "$BUCKLESCRIPT" = YES ]
 then
     bash ./test/ci/travis-bucklescript.sh
