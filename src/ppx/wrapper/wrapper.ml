2adc2109f7c2f4c096a2e13520173f56835a9b6c
Anton Bachin <antonbachin@yahoo.com>
2019-09-03 17:19:00 +0300

    BuckleScript: link Bisect_ppx into wrapper
    Resolves #248.

--- src/ppx/wrapper/wrapper.ml
+++ src/ppx/wrapper/wrapper.ml
@@ -1,28 +1,5 @@
 let () =
-  let rec find_node_modules path =
-    match path with
-    | "" -> None
-    | _ when path = Filename.current_dir_name -> None
-    | _ when path = Filename.dir_sep -> None
-    | _ ->
-      if Filename.basename path = "node_modules" then
-        Some path
-      else
-        find_node_modules (Filename.dirname path)
-  in
-
-  let (//) = Filename.concat in
-  let ppx_exe = "_build/install/default/lib/bisect_ppx/ppx.exe" in
-
-  let path_to_ppx =
-    match find_node_modules (Sys.argv.(0)) with
-    | Some node_modules ->
-      node_modules // "@aantron/bisect_ppx" // ppx_exe
-    | None ->
-      Path.build_dir // Path.to_root // "../.." // ppx_exe
-  in
-
-  let arguments =
+  let argv =
     match Array.to_list Sys.argv with
     | [program; input_file; output_file] ->
       Array.of_list [
@@ -35,4 +12,5 @@ let () =
     | _ ->
       Sys.argv
   in
-  Unix.execv path_to_ppx arguments
+
+  Migrate_parsetree.Driver.run_main ~argv ()
