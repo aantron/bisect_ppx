22f056dda1962d14d9f4c505836f612dd01b8c34
Anton Bachin <antonbachin@yahoo.com>
2020-04-19 17:42:26 +0300

    Support taking PPX options from BuckleScript
    Fixes #310.

--- src/ppx/bucklescript/ppx.ml
+++ src/ppx/bucklescript/ppx.ml
@@ -1,16 +1,10 @@
 let () =
+  let argc = Array.length Sys.argv in
   let argv =
-    match Sys.argv with
-    | [|program; input_file; output_file|] ->
-      [|
-        program;
-        input_file;
-        "-o"; output_file;
-        "--dump-ast";
-        "--conditional";
-      |]
-    | _ ->
-      Sys.argv
+    let output_file = Sys.argv.(argc - 1) in
+    Array.(append
+      (sub Sys.argv 0 (argc - 1))
+      [|"-o"; output_file; "--dump-ast"; "--conditional"|])
   in
 
   Migrate_parsetree.Driver.run_main ~argv ()
