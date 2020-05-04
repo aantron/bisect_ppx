fb04f22027674beb8e69277e3c9ce216d95fe7bc
jihchi <achi@987.tw>
2020-05-04 15:05:16 +0800

    Add a comment

--- src/runtime/bucklescript/jest.ml
+++ src/runtime/bucklescript/jest.ml
@@ -1,3 +1,10 @@
+(* If we make a curried binding of `afterAll`, BuckleScript will compiles it like this:
+  ```
+    afterAll((function (param) { // <-- notice the `param` parameter here
+      // ... snip ...
+    }));
+  ```
+  this will cause timeout error in Jest because Jest will wait until you manully call `done` from `param`. *)
 external afterAll : ((unit -> unit)[@bs ]) -> unit = "afterAll"[@@bs.val ]
 
 let () =
