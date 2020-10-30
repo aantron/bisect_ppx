ab678755cc93a682a9757ca0dff1b43dcabecfba
Anton Bachin <antonbachin@yahoo.com>
2020-10-30 10:21:04 +0300

    Do not seed the global random number generator
    Fixes #344.

--- src/common/bisect_common.ml
+++ src/common/bisect_common.ml
@@ -171,11 +171,12 @@ let write_runtime_data channel =
   in
   output_string channel data
 
-let () =
-  Random.self_init () [@coverage off]
+let prng =
+  Random.State.make_self_init () [@coverage off]
 
 let random_filename base_name =
-  Printf.sprintf "%s%09d.coverage" base_name (abs (Random.int 1000000000))
+  Printf.sprintf "%s%09d.coverage"
+    base_name (abs (Random.State.int prng 1000000000))
 
 let write_points points =
   let points_array = Array.of_list points in
