f77afe47713e4c5dbbba15e640b7ee93cb9e1406
Anton Bachin <antonbachin@yahoo.com>
2021-07-19 20:43:41 +0300

    Commit binaries from GitHub Actions

--- test/ci/travis-binaries.sh
+++ test/ci/travis-binaries.sh
@@ -5,7 +5,9 @@ set -e
 git remote set-url origin git@github.com:aantron/bisect_ppx.git
 git config user.name "Anton Bachin"
 git config user.email "antonbachin@yahoo.com"
-echo $DEPLOY_KEY | base64 --decode > ~/.ssh/binaries
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+echo "$DEPLOY_KEY" | base64 --decode > ~/.ssh/binaries
 chmod 400 ~/.ssh/binaries
 echo >> ~/.ssh/config
 echo "Host github.com" >> ~/.ssh/config
@@ -15,19 +17,19 @@ echo "  StrictHostKeyChecking no" >> ~/.ssh/config
 set +e
 set -x
 
-case $TRAVIS_OS_NAME in
-    "linux") OS=linux;;
-    "osx") OS=macos;;
-    *) echo Unsupported system $TRAVIS_OS_NAME; exit 1;;
+case $RUNNER_OS in
+    "Linux") OS=linux;;
+    "macOS") OS=macos;;
+    *) echo Unsupported system $RUNNER_OS; exit 1;;
 esac
 
 try_to_commit() {
     git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
     git fetch --unshallow origin
     git checkout -b binaries origin/binaries || git checkout -b binaries
-    if ! git merge-base --is-ancestor $TRAVIS_COMMIT binaries
+    if ! git merge-base --is-ancestor $GITHUB_SHA binaries
     then
-        git reset --hard $TRAVIS_COMMIT
+        git reset --hard $GITHUB_SHA
     fi
     mkdir -p bin/$OS
     cp test/bucklescript/node_modules/bisect_ppx/ppx bin/$OS/
@@ -44,7 +46,7 @@ try_to_commit() {
     git commit -F commit-message
     git push --force-with-lease -u origin binaries
     RESULT=$?
-    git checkout $TRAVIS_BRANCH
+    git checkout $GITHUB_REF
     git branch -D binaries
     return $RESULT
 }
