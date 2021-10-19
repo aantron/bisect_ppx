306f8b8d0f47ff88e1e0156a0ecb18d7283a5d7d
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2021-10-19 16:48:01 +0200

    Type's representation of the Cobertura's XML

--- src/report/report_cobertura.ml
+++ src/report/report_cobertura.ml
@@ -4,4 +4,36 @@
 
 module Common = Bisect_common
 
+type source = string
+
+type line = { number : int; hits : int }
+
+type class_ = {
+  name : string;
+  complexity : int;
+  line_rate : float;
+  branch_rate : int;
+  lines : line list;
+}
+
+type package = {
+  name : string;
+  line_rate : float;
+  branch_rate : float;
+  complexity : int;
+  classes : class_ list
+}
+
+type cobertura = {
+  lines_valid : int;
+  lines_covered : int;
+  line_rate : float;
+  branches_covered : int;
+  branches_valid : int;
+  branch_rate : float;
+  complexity : int;
+  sources : source list;
+  package : package;
+}
+
 let output _verbose _file _resolver _data _points = failwith "todo"
