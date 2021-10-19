f0b24b6ceb0664ce4ce0c19a77f6d6a54f9a4b72
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2021-10-19 16:46:24 +0200

    New rule for Cobertura

---
+++ src/report/report_cobertura.ml
@@ -0,0 +1,7 @@
+(* This file is part of Bisect_ppx, released under the MIT license. See
+   LICENSE.md for details, or visit
+   https://github.com/aantron/bisect_ppx/blob/master/LICENSE.md. *)
+
+module Common = Bisect_common
+
+let output _verbose _file _resolver _data _points = failwith "todo"
