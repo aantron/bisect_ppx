dbba6891577e67ddfe86fbe78b3e5893223aab14
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2021-10-20 11:08:59 +0200

    Remove complexity and branches's information

--- src/report/report_cobertura.ml
+++ src/report/report_cobertura.ml
@@ -10,17 +10,13 @@ type line = { number : int; hits : int }
 
 type class_ = {
   name : string;
-  complexity : int;
   line_rate : float;
-  branch_rate : int;
   lines : line list;
 }
 
 type package = {
   name : string;
   line_rate : float;
-  branch_rate : float;
-  complexity : int;
   classes : class_ list
 }
 
@@ -28,10 +24,6 @@ type cobertura = {
   lines_valid : int;
   lines_covered : int;
   line_rate : float;
-  branches_covered : int;
-  branches_valid : int;
-  branch_rate : float;
-  complexity : int;
   sources : source list;
   package : package;
 }
@@ -44,15 +36,13 @@ let pp_lines fmt lines =
   fprintf fmt "<lines>@;<0 4>@[<v 4>    %a@]@;<0 4></lines>"
     (pp_print_list pp_line) lines
 
-let pp_class_ fmt {name; complexity; line_rate; branch_rate; lines} =
+let pp_class_ fmt {name; line_rate; lines} =
   let open Format in
   let class_infos =
-    Format.sprintf {|name="%s" filename="%s" complexity="%d" line-rate="%f" branch-rate="%d"|}
+    Format.sprintf {|name="%s" filename="%s" line-rate="%f"|}
       name
       name
-      complexity
       line_rate
-      branch_rate
   in
   fprintf fmt
     "<class %s>@;<0 4><methods/>@;<0 4>%a@;<0 0></class>"
@@ -65,14 +55,12 @@ let pp_classes fmt classes =
     "<classes>@;<0 4>@[<v 0>%a@]@;</classes>"
     (pp_print_list pp_class_) classes
 
-let pp_package fmt {name; line_rate; branch_rate; complexity; classes } =
+let pp_package fmt {name; line_rate; classes } =
   let open Format in
   let package_infos =
-    Format.sprintf {|name="%s" line-rate="%f" branch-rate="%f" complexity="%d"|}
+    Format.sprintf {|name="%s" line-rate="%f"|}
       name
       line_rate
-      branch_rate
-      complexity
   in
   fprintf fmt {|<package %s>@;<0 4>@[<v 0>%a@]@;</package>|}
     package_infos
@@ -93,20 +81,12 @@ let pp_cobertura fmt ({sources; package; _} as cobertura) =
       lines_valid;
       lines_covered;
       line_rate;
-      branches_covered;
-      branches_valid;
-      branch_rate;
-      complexity;
       _ } =
     sprintf
-      {|lines-valid="%d" lines-covered="%d" line-rate="%f" branches-covered="%d" branches-valid="%d" branch-rate="%f" complexity="%d"|}
+      {|lines-valid="%d" lines-covered="%d" line-rate="%f"|}
       lines_valid
       lines_covered
       line_rate
-      branches_covered
-      branches_valid
-      branch_rate
-      complexity
   in
   fprintf fmt
     {|<?xml version="1.0" ?>@.<coverage %s>@;<0 4>@[<v 0>%a@]@;<0 4>@[<v 0>%a@]@;</coverage>@.|}
@@ -155,7 +135,7 @@ let classes ~global_counts verbose data resolver points : class_ list =
           line_counts
       in
 
-      Some ({name = in_file; complexity = 0; line_rate; branch_rate = 0; lines})
+      Some ({name = in_file; line_rate; lines})
   in
 
   Hashtbl.fold
@@ -168,7 +148,7 @@ let package ~counts ~verbose ~data ~resolver ~points =
   let classes = classes ~global_counts:counts verbose data resolver points in
   let line_rate = line_rate counts in
 
-  { name = "."; line_rate; branch_rate = 0.0; complexity = 0; classes}
+  { name = "."; line_rate; classes}
 
 let cobertura ~verbose ~data ~resolver ~points =
   let counts = Report_utils.make () in
@@ -179,10 +159,6 @@ let cobertura ~verbose ~data ~resolver ~points =
     lines_valid = counts.total;
     lines_covered = counts.visited;
     line_rate = rate;
-    branches_covered = 0;
-    branches_valid = 0;
-    branch_rate = 0.0;
-    complexity = 0;
     package;
     sources;
   }
