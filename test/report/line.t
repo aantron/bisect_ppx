aantron/bisect_ppx

commit 1b8d7ec5985aa12a85e797e3d53fc72713e80c35

Clarify and simplify .coverage I/O functions
- Use clearer types.
- Don't do staged reading. Convert all the data in one place.
- Eliminate remaining uses of Marshal, accordingly also bump the magic
  number (file header identifier).
- Don't use an intermediate buffer when writing.
- Don't store point index (it is implicit in the point's position within
  the points array).

See #111 for an older proposal for similar .coverage types.

src/report/html.ml
@@ -143,20 +143,20 @@ let output_html tab_size title theme in_file out_file resolver visited points =
     Util.info "... file not found";
     None
   | Some resolved_in_file ->
-    let cmp_content = Hashtbl.find points in_file |> Util.read_points in
+    let cmp_content = Hashtbl.find points in_file in
     Util.info "... file has %d points" (List.length cmp_content);
     let len = Array.length visited in
     let stats = Util.make () in
     let pts =
-      ref (cmp_content |> List.map (fun p ->
+      ref (cmp_content |> List.mapi (fun index offset ->
         let nb =
-          if Bisect_common.(p.identifier) < len then
-            visited.(Bisect_common.(p.identifier))
+          if index < len then
+            visited.(index)
           else
             0
         in
         Util.update stats (nb > 0);
-        (Bisect_common.(p.offset), nb)))
+        (offset, nb)))
     in
     let dirname, basename = split_filename in_file in
     let in_channel, out_channel = Util.open_both resolved_in_file out_file in

src/report/util.ml
@@ -98,25 +98,19 @@ let add counts_1 counts_2 =
   {visited = counts_1.visited ++ counts_2.visited;
    total = counts_1.total ++ counts_2.total}
 
-let read_points s =
-  let points_array : Bisect_common.point_definition array =
-    Marshal.from_string s 0 in
-  Array.sort compare points_array;
-  Array.to_list points_array
-
 let line_counts in_file resolved_in_file visited points =
-  let cmp_content = Hashtbl.find points in_file |> read_points in
+  let cmp_content = Hashtbl.find points in_file in
   info "... file has %d points" (List.length cmp_content);
   let len = Array.length visited in
   let pts =
-    cmp_content |> List.map (fun p ->
+    cmp_content |> List.mapi (fun index offset ->
       let nb =
-        if Bisect_common.(p.identifier) < len then
-          visited.(Bisect_common.(p.identifier))
+        if index < len then
+          visited.(index)
         else
           0
       in
-      (Bisect_common.(p.offset), nb))
+      (offset, nb))
   in
   let in_channel = open_in resolved_in_file in
   let line_counts =

src/common/bisect_common.ml
@@ -4,85 +4,70 @@
 
 
 
-type point_definition = {
-    offset : int;
-    identifier : int;
-  }
+type instrumented_file = {
+  filename : string;
+  points : int list;
+  counts : int array;
+}
 
-type coverage_data = (string * (int array * string)) array
+type coverage =
+  instrumented_file list
 
 
 
-(* I/O functions *)
+let coverage_file_identifier = "BISECT-COVERAGE-4"
 
-let magic_number_rtd = "BISECTOUT3"
+let write_int formatter i =
+  Format.fprintf formatter " %i" i
 
-module Writer :
-sig
-  type 'a t
+let write_string formatter s =
+  Format.fprintf formatter " %i %s" (String.length s) s
 
-  val int : int t
-  val string : string t
-  val pair : 'a t -> 'b t -> ('a * 'b) t
-  val array : 'a t -> 'a array t
+let write_array write_element formatter a =
+  Format.fprintf formatter " %i" (Array.length a);
+  Array.iter (write_element formatter) a
 
-  val write : 'a t -> 'a -> string
-end =
-struct
-  type 'a t = Buffer.t -> 'a -> unit
+let write_list write_element formatter l =
+  Format.fprintf formatter " %i" (List.length l);
+  List.iter (write_element formatter) l
 
-  let w =
-    Printf.bprintf
+let write_instrumented_file formatter {filename; points; counts} =
+  write_string formatter filename;
+  write_list write_int formatter points;
+  write_array write_int formatter counts
 
-  let int b i =
-    w b " %i" i
+let write_coverage formatter coverage =
+  Format.fprintf formatter "%s" coverage_file_identifier;
+  write_list write_instrumented_file formatter coverage;
+  Format.pp_print_flush formatter ()
 
-  let string b s =
-    w b " %i %s" (String.length s) s
 
-  let pair left right b (l, r) =
-    left b l;
-    right b r
 
-  let array element b a =
-    w b " %i" (Array.length a);
-    Array.iter (element b) a
-
-  let write writer v =
-    let b = Buffer.create 4096 in
-    Buffer.add_string b magic_number_rtd;
-    writer b v;
-    Buffer.contents b
-end
-
-let table : (string, int array * string) Hashtbl.t Lazy.t =
+let table : (string, instrumented_file) Hashtbl.t Lazy.t =
   lazy (Hashtbl.create 17)
 
 let reset_counters () =
   Lazy.force table
-  |> Hashtbl.iter begin fun _ (point_state, _) ->
-    match Array.length point_state with
+  |> Hashtbl.iter begin fun _ {counts; _} ->
+    match Array.length counts with
     | 0 -> ()
-    | n -> Array.fill point_state 0 (n - 1) 0
+    | n -> Array.fill counts 0 (n - 1) 0
   end
 
+let flatten_data () =
+  Hashtbl.fold (fun _ file acc -> file::acc) (Lazy.force table) []
+
 let runtime_data_to_string () =
-  let data = Hashtbl.fold (fun k v acc -> (k, v)::acc) (Lazy.force table) [] in
-  match data with
+  match flatten_data () with
   | [] ->
     None
-  | _ ->
-    (Array.of_list data : coverage_data)
-    |> Writer.(write (array (pair string (pair (array int) string))))
-    |> fun s -> Some s
+  | data ->
+    let buffer = Buffer.create 4096 in
+    write_coverage (Format.formatter_of_buffer buffer) data;
+    Some (Buffer.contents buffer)
 
 let write_runtime_data channel =
-  let data =
-    match runtime_data_to_string () with
-    | Some s -> s
-    | None -> Writer.(write (array int)) [||]
-  in
-  output_string channel data
+  write_coverage (Format.formatter_of_out_channel channel) (flatten_data ())
 
 let prng =
   Random.State.make_self_init () [@coverage off]
@@ -91,18 +76,15 @@ let random_filename base_name =
   Printf.sprintf "%s%09d.coverage"
     base_name (abs (Random.State.int prng 1000000000))
 
-let register_file file ~point_count ~point_definitions =
-  let point_state = Array.make point_count 0 in
+let register_file ~filename ~points =
+  let counts = Array.make (List.length points) 0 in
   let table = Lazy.force table in
-  if not (Hashtbl.mem table file) then
-    Hashtbl.add table file (point_state, point_definitions);
-  `Staged (fun point_index ->
-    let current_count = point_state.(point_index) in
-    point_state.(point_index) <-
-      if current_count < max_int then
-        current_count + 1
-      else
-        current_count)
+  if not (Hashtbl.mem table filename) then
+    Hashtbl.add table filename {filename; points; counts};
+  `Visit (fun index ->
+    let current_count = counts.(index) in
+    if current_count < max_int then
+      counts.(index) <- current_count + 1)
 
 
 

src/common/bisect_common.mli
@@ -8,20 +8,21 @@
     various parts of Bisect. *)
 
 
-type point_definition = {
-    offset : int; (** Point offset, relative to file beginning. *)
-    identifier : int; (** Point identifier, unique in file. *)
-  }
-(** The type of point definitions, that is places of interest in the
-    source code. *)
 
-type coverage_data = (string * (int array * string)) array
+type instrumented_file = {
+  filename : string;
+  points : int list; (** Byte offsets of the points placed in the file. *)
+  counts : int array; (** Visitation counts, one for each point. *)
+}
+
+type coverage =
+  instrumented_file list
 
 
 
 (** {1 I/O functions} *)
 
-val magic_number_rtd : string
+val coverage_file_identifier : string
 
 val write_runtime_data : out_channel -> unit
 (** [write_runtime_data o] writes the current runtime data to the output
@@ -44,8 +45,7 @@ val reset_counters : unit -> unit
 (** Clears accumulated coverage statistics. *)
 
 val register_file :
-  string -> point_count:int -> point_definitions:string ->
-    [`Staged of (int -> unit)]
+  filename:string -> points:int list -> [`Visit of (int -> unit)]
 (** [register_file file ~point_count ~point_definitions] indicates that the file
     [file] is part of the application that has been instrumented.
     [point_definitions] is a serialized [Common.point_definition list] giving

src/ppx/instrument.ml
@@ -45,8 +45,6 @@ module Str = Ppxlib.Ast_helper.Str
 module Cl = Ppxlib.Ast_helper.Cl
 module Cf = Ppxlib.Ast_helper.Cf
 
-module Common = Bisect_common
-
 
 
 (* Can be removed once Bisect_ppx requires OCaml >= 4.08. *)
@@ -141,9 +139,15 @@ sig
     points -> string -> Parsetree.structure_item list
 end =
 struct
-  type points = Common.point_definition list ref
+  type points = {
+    mutable offsets : int list;
+    mutable count : int;
+  }
 
-  let init () = ref []
+  let init () = {
+    offsets = [];
+    count = 0;
+  }
 
   (* Given an AST for an expression [e], replaces it by the sequence expression
      [instrumentation; e], where [instrumentation] is some code that tells
@@ -191,18 +195,19 @@ struct
           Location.(Lexing.(loc.loc_end.pos_cnum - 1))
       in
       let point =
-        try
-          List.find
-            (fun point -> Common.(point.offset) = point_offset)
-            !points
-        with Not_found ->
-          let new_index = List.length !points in
-          let new_point =
-            Common.{offset = point_offset; identifier = new_index} in
-          points := new_point::!points;
-          new_point
+        let rec find_point points offset index offsets =
+          match offsets with
+          | offset'::_ when offset' = offset -> index
+          | _::rest -> find_point points offset (index - 1) rest
+          | [] ->
+            let index = points.count in
+            points.offsets <- offset::points.offsets;
+            points.count <- points.count + 1;
+            index
+        in
+        find_point points point_offset (points.count - 1) points.offsets
       in
-      Ast_builder.Default.eint ~loc point.identifier
+      Ast_builder.Default.eint ~loc point
 
     in
 
@@ -865,11 +870,6 @@ struct
     |> fun (v, e, f, n, _) ->
       List.rev v, List.rev e, List.rev f, n && not use_aliases
 
-  let write_points points =
-    let points_array = Array.of_list points in
-    Array.sort compare points_array;
-    Marshal.to_string points_array []
-
   let runtime_initialization points file =
     let loc = Location.in_file file in
 
@@ -883,9 +883,18 @@ struct
       "Bisect_visit___" ^ (Buffer.contents buffer)
     in
 
-    let point_count = Ast_builder.Default.eint ~loc (List.length !points) in
-    let points_data = Ast_builder.Default.estring ~loc (write_points !points) in
-    let file = Ast_builder.Default.estring ~loc file in
+    let points_data =
+      let open Parsetree in
+        let rec inline_point_offsets acc = function
+        | [] -> acc
+        | offset::more ->
+          inline_point_offsets
+            [%expr [%e Ast_builder.Default.eint ~loc offset]::[%e acc]]
+            more
+      in
+      inline_point_offsets [%expr []] points.offsets
+    in
+    let filename = Ast_builder.Default.estring ~loc file in
 
     let ast_convenience_str_opt = function
       | None ->
@@ -894,8 +903,8 @@ struct
         Some (Ast_builder.Default.estring ~loc v)
         |> Exp.construct ~loc {txt = Longident.parse "Some"; loc}
     in
-    let bisect_file = ast_convenience_str_opt !Common.bisect_file in
-    let bisect_silent = ast_convenience_str_opt !Common.bisect_silent in
+    let bisect_file = ast_convenience_str_opt !Bisect_common.bisect_file in
+    let bisect_silent = ast_convenience_str_opt !Bisect_common.bisect_silent in
 
     (* ___bisect_visit___ is a function with a reference to a point count array.
        It is called every time a point is visited.
@@ -971,13 +980,13 @@ struct
         let open Parsetree in
         [%stri
           let ___bisect_visit___ =
-            let point_definitions = [%e points_data] in
-            let `Staged cb =
+            let points = [%e points_data] in
+            let `Visit visit =
               Bisect.Runtime.register_file
                 ~bisect_file:[%e bisect_file] ~bisect_silent:[%e bisect_silent]
-                [%e file] ~point_count:[%e point_count] ~point_definitions
+                ~filename:[%e filename] ~points
             in
-            cb
+            visit
         ]
       in
 

src/report/input.ml
@@ -146,72 +146,66 @@ let try_in_channel bin x f =
 (* filename + reason *)
 exception Invalid_file of string * string
 
-module Reader :
-sig
-  type 'a t
-
-  val int : int t
-  val string : string t
-  val pair : 'a t -> 'b t -> ('a * 'b) t
-  val array : 'a t -> 'a array t
-
-  val read : 'a t -> filename:string -> 'a
-end =
-struct
-  type 'a t = Buffer.t -> in_channel -> 'a
-
-  let junk c =
-    try ignore (input_char c)
+  let junk channel =
+    try ignore (input_char channel)
     with End_of_file -> ()
 
-  let int b c =
-    Buffer.clear b;
+  let read_int buffer channel =
+    Buffer.clear buffer;
     let rec loop () =
-      match input_char c with
+      match input_char channel with
       | exception End_of_file -> ()
       | ' ' -> ()
-      | c -> Buffer.add_char b c; loop ()
+      | c -> Buffer.add_char buffer c; loop ()
     in
     loop ();
-    int_of_string (Buffer.contents b)
+    int_of_string (Buffer.contents buffer)
 
-  let string b c =
-    let length = int b c in
-    let s = really_input_string c length in
-    junk c;
-    s
+  let read_string buffer channel =
+    let length = read_int buffer channel in
+    let string = really_input_string channel length in
+    junk channel;
+    string
 
-  let pair left right b c =
-    let l = left b c in
-    let r = right b c in
-    l, r
+  let read_array read_element buffer channel =
+    let length = read_int buffer channel in
+    Array.init length (fun _index -> read_element buffer channel)
 
-  let array element b c =
-    let length = int b c in
-    Array.init length (fun _index -> element b c)
+let read_list read_element buffer channel =
+  read_array read_element buffer channel |> Array.to_list
 
-  let read reader ~filename =
+let read_instrumented_file buffer channel =
+  let filename = read_string buffer channel in
+  let points = read_list read_int buffer channel in
+  let counts = read_array read_int buffer channel in
+  Bisect_common.{filename; points; counts}
+
+let read_coverage buffer channel =
+  read_list read_instrumented_file buffer channel
+
+  let read ~filename =
     try_in_channel true filename begin fun c ->
       let magic_number_in_file =
-        try really_input_string c (String.length Bisect_common.magic_number_rtd)
+        try
+          really_input_string
+            c (String.length Bisect_common.coverage_file_identifier)
         with End_of_file ->
           raise
             (Invalid_file
               (filename, "unexpected end of file while reading magic number"))
       in
-      if magic_number_in_file <> Bisect_common.magic_number_rtd then
+      if magic_number_in_file <> Bisect_common.coverage_file_identifier then
         raise (Invalid_file (filename, "bad magic number"));
 
       junk c;
 
       let b = Buffer.create 4096 in
-      try reader b c
+      try read_coverage b c
       with e ->
         raise
           (Invalid_file
             (filename, "exception reading data: " ^ Printexc.to_string e))
     end
-end
 
 let get_relative_path file =
   if Filename.is_relative file then
@@ -231,32 +225,30 @@ let get_relative_path file =
       file
 
 let read_runtime_data filename =
-  let data : Bisect_common.coverage_data =
-    Reader.(read (array (pair string (pair (array int) string)))) ~filename in
-  data
-  |> Array.to_list
-  |> List.map (fun (file, data) -> get_relative_path file, data)
+  read ~filename
+  |> List.map (fun file ->
+    Bisect_common.{file with filename = get_relative_path file.filename})
 
 let load_coverage files search_paths expect do_not_expect =
   let data, points =
     let total_counts = Hashtbl.create 17 in
-    let points = Hashtbl.create 17 in
+    let all_points = Hashtbl.create 17 in
 
     Coverage_input_files.list files search_paths
     |> List.iter (fun out_file ->
       read_runtime_data out_file
-      |> List.iter (fun (source_file, (file_counts, file_points)) ->
+      |> List.iter (fun Bisect_common.{filename; points; counts} ->
         let file_counts =
           try
             Util.elementwise_saturation_addition
-              (Hashtbl.find total_counts source_file)
-              file_counts
-          with Not_found -> file_counts
+              (Hashtbl.find total_counts filename)
+              counts
+          with Not_found -> counts
         in
-        Hashtbl.replace total_counts source_file file_counts;
-        Hashtbl.replace points source_file file_points));
+        Hashtbl.replace total_counts filename file_counts;
+        Hashtbl.replace all_points filename points));
 
-    total_counts, points
+    total_counts, all_points
   in
 
   let present_files =

src/report/input.mli
@@ -6,4 +6,4 @@
 
 val load_coverage :
   string list -> string list -> string list -> string list ->
-    (string, int array) Hashtbl.t * (string, string) Hashtbl.t
+    (string, int array) Hashtbl.t * (string, int list) Hashtbl.t

commit 3ce4f92c245aa0d33f335dda433278912e80fa4b

HTML, Coveralls, Cobertura: fix point display bug
Introduced in 1b8d7ec5985aa12a85e797e3d53fc72713e80c35, when points were
no longer being sorted by offset during loading. Points are now sorted
by offset again. When not sorted, the reporter is not able to display
points with a higher index but lower offset, because it relies on the
offsets being in increasing order as it walks through files, "merging"
the point data into files.

src/report/html.ml
@@ -160,8 +160,13 @@ let output_for_source_file
 
   let len = Array.length counts in
   let stats = ref (0, 0) in
+  let points =
+    points
+    |> List.mapi (fun index offset -> (offset, index))
+    |> List.sort compare
+  in
   let pts =
-    ref (points |> List.mapi (fun index offset ->
+    ref (points |> List.map (fun (offset, index) ->
       let nb =
         if index < len then
           counts.(index)

src/report/util.ml
@@ -68,8 +68,13 @@ let find_file ~source_roots ~ignore_missing_files ~filename =
 let line_counts ~filename ~points ~counts =
   info "... file has %d points" (List.length points);
   let len = Array.length counts in
+  let points =
+    points
+    |> List.mapi (fun index offset -> (offset, index))
+    |> List.sort compare
+  in
   let pts =
-    points |> List.mapi (fun index offset ->
+    points |> List.map (fun (offset, index) ->
       let nb =
         if index < len then
           counts.(index)

test/report/html.t
@@ -25,14 +25,14 @@
     <body>
       <div id="header">
         <h1>Coverage report</h1>
-        <h2>66.66%</h2>
+        <h2>33.33%</h2>
       </div>
       <div id="files">
         <div>
           <span class="meter">
-            <span class="covered" style="width: 66%"></span>
+            <span class="covered" style="width: 33%"></span>
           </span>
-          <span class="percentage">66%</span>
+          <span class="percentage">33%</span>
           <a href="test.ml.html">
             <span class="dirname"></span>test.ml
           </a>
@@ -57,10 +57,13 @@
             <span class="dirname"></span>test.ml
           </a>
         </h1>
-        <h2>66.67%</h2>
+        <h2>33.33%</h2>
       </div>
       <div id="navbar">
-        <span class="unvisited" style="bottom:37.50%"></span>
+        <span class="unvisited" style="top:31.25%"></span>
+        <span class="unvisited" style="bottom:18.75%"></span>
+        <span class="unvisited" style="bottom:12.50%"></span>
+        <span class="unvisited" style="bottom:0.00%"></span>
       </div>
       <div id="report">
         <div id="lines-layer">
@@ -73,18 +76,34 @@
   <a id="L6"></a><span > </span>
   <a id="L7"></a><span > </span>
   <a id="L8"></a><span class="visited"> </span>
+  <a id="L9"></a><span > </span>
+  <a id="L10"></a><span > </span>
+  <a id="L11"></a><span > </span>
+  <a id="L12"></a><span > </span>
+  <a id="L13"></a><span class="unvisited"> </span>
+  <a id="L14"></a><span class="unvisited"> </span>
+  <a id="L15"></a><span > </span>
+  <a id="L16"></a><span class="unvisited"> </span>
   </pre>
         </div>
         <div id="text-layer">
           <pre id="line-numbers">
-  <a href="#L1">1</a>
-  <a href="#L2">2</a>
-  <a href="#L3">3</a>
-  <a href="#L4">4</a>
-  <a href="#L5">5</a>
-  <a href="#L6">6</a>
-  <a href="#L7">7</a>
-  <a href="#L8">8</a>
+  <a href="#L1"> 1</a>
+  <a href="#L2"> 2</a>
+  <a href="#L3"> 3</a>
+  <a href="#L4"> 4</a>
+  <a href="#L5"> 5</a>
+  <a href="#L6"> 6</a>
+  <a href="#L7"> 7</a>
+  <a href="#L8"> 8</a>
+  <a href="#L9"> 9</a>
+  <a href="#L10">10</a>
+  <a href="#L11">11</a>
+  <a href="#L12">12</a>
+  <a href="#L13">13</a>
+  <a href="#L14">14</a>
+  <a href="#L15">15</a>
+  <a href="#L16">16</a>
   </pre>
   <pre><code class="ocaml">let f () =
     <span data-count="1">(</span>)
@@ -94,6 +113,14 @@
   
   let () =
     <span data-count="1">f</span> ()
+  
+  (* Reproduces a HTML display bug that existed in development between 2.6.3 and
+     2.7.0, starting with 1b8d7ec5985aa12a85e797e3d53fc72713e80c35. *)
+  let a () =
+    <span data-count="0">i</span>f true then
+      <span data-count="0">t</span>rue
+    else
+      <span data-count="0">f</span>alse
   </code></pre>
         </div>
       </div>

test/report/line.t
@@ -26,6 +26,14 @@ Reporter still works even in the presence of line number directives.
   let () =
     f ()
   
+  (* Reproduces a HTML display bug that existed in development between 2.6.3 and
+     2.7.0, starting with 1b8d7ec5985aa12a85e797e3d53fc72713e80c35. *)
+  let a () =
+    if true then
+      true
+    else
+      false
+  
   # 1 "other_file.ml"
   
   let h () =
@@ -54,10 +62,13 @@ Reporter still works even in the presence of line number directives.
             <span class="dirname"></span>test2.ml
           </a>
         </h1>
-        <h2>80.00%</h2>
+        <h2>50.00%</h2>
       </div>
       <div id="navbar">
-        <span class="unvisited" style="top:31.25%"></span>
+        <span class="unvisited" style="top:20.83%"></span>
+        <span class="unvisited" style="bottom:45.83%"></span>
+        <span class="unvisited" style="bottom:41.67%"></span>
+        <span class="unvisited" style="bottom:33.33%"></span>
       </div>
       <div id="report">
         <div id="lines-layer">
@@ -74,10 +85,18 @@ Reporter still works even in the presence of line number directives.
   <a id="L10"></a><span > </span>
   <a id="L11"></a><span > </span>
   <a id="L12"></a><span > </span>
-  <a id="L13"></a><span class="visited"> </span>
-  <a id="L14"></a><span > </span>
+  <a id="L13"></a><span class="unvisited"> </span>
+  <a id="L14"></a><span class="unvisited"> </span>
   <a id="L15"></a><span > </span>
-  <a id="L16"></a><span class="visited"> </span>
+  <a id="L16"></a><span class="unvisited"> </span>
+  <a id="L17"></a><span > </span>
+  <a id="L18"></a><span > </span>
+  <a id="L19"></a><span > </span>
+  <a id="L20"></a><span > </span>
+  <a id="L21"></a><span class="visited"> </span>
+  <a id="L22"></a><span > </span>
+  <a id="L23"></a><span > </span>
+  <a id="L24"></a><span class="visited"> </span>
   </pre>
         </div>
         <div id="text-layer">
@@ -98,6 +117,14 @@ Reporter still works even in the presence of line number directives.
   <a href="#L14">14</a>
   <a href="#L15">15</a>
   <a href="#L16">16</a>
+  <a href="#L17">17</a>
+  <a href="#L18">18</a>
+  <a href="#L19">19</a>
+  <a href="#L20">20</a>
+  <a href="#L21">21</a>
+  <a href="#L22">22</a>
+  <a href="#L23">23</a>
+  <a href="#L24">24</a>
   </pre>
   <pre><code class="ocaml">let f () =
     <span data-count="1">(</span>)
@@ -107,6 +134,14 @@ Reporter still works even in the presence of line number directives.
   
   let () =
     <span data-count="1">f</span> ()
+  
+  (* Reproduces a HTML display bug that existed in development between 2.6.3 and
+     2.7.0, starting with 1b8d7ec5985aa12a85e797e3d53fc72713e80c35. *)
+  let a () =
+    <span data-count="0">i</span>f true then
+      <span data-count="0">t</span>rue
+    else
+      <span data-count="0">f</span>alse
   
   # 1 "other_file.ml"
   

test/report/send-to.t
@@ -49,7 +49,7 @@ From Travis to Coveralls.
   Info: using job ID variable $TRAVIS_JOB_ID
   Info: found coverage files in './'
   Info: Processing file 'test.ml'...
-  Info: ... file has 3 points
+  Info: ... file has 6 points
   Info: sending to Coveralls with command:
   Info: curl -L -F json_file=@./coverage.json https://coveralls.io/api/v1/jobs
   $ cat coverage.json
@@ -61,8 +61,8 @@ From Travis to Coveralls.
     "source_files": [
           {
               "name": "test.ml",
-              "source_digest": "55ec0167abb99a3141afb092c92d0e23",
-              "coverage": [null,1,null,null,0,null,null,1]
+              "source_digest": "c006f07c5cf67adcdb328acb8f085dd9",
+              "coverage": [null,1,null,null,0,null,null,1,null,null,null,null,0,0,null,0]
           }
     ]
   }
@@ -77,7 +77,7 @@ From Travis to Codecov.
   Info: using job ID variable $TRAVIS_JOB_ID
   Info: found coverage files in './'
   Info: Processing file 'test.ml'...
-  Info: ... file has 3 points
+  Info: ... file has 6 points
   Info: sending to Codecov with command:
   Info: curl -s https://codecov.io/bash | bash -s -- -Z -f coverage.json
   $ cat coverage.json
@@ -89,8 +89,8 @@ From Travis to Codecov.
     "source_files": [
           {
               "name": "test.ml",
-              "source_digest": "55ec0167abb99a3141afb092c92d0e23",
-              "coverage": [null,1,null,null,0,null,null,1]
+              "source_digest": "c006f07c5cf67adcdb328acb8f085dd9",
+              "coverage": [null,1,null,null,0,null,null,1,null,null,null,null,0,0,null,0]
           }
     ]
   }
@@ -134,7 +134,7 @@ From CircleCI to Coveralls.
   Info: including git info
   Info: found coverage files in './'
   Info: Processing file 'test.ml'...
-  Info: ... file has 3 points
+  Info: ... file has 6 points
   Info: sending to Coveralls with command:
   Info: curl -L -F json_file=@./coverage.json https://coveralls.io/api/v1/jobs
   $ cat coverage.json
@@ -148,8 +148,8 @@ From CircleCI to Coveralls.
     "source_files": [
           {
               "name": "test.ml",
-              "source_digest": "55ec0167abb99a3141afb092c92d0e23",
-              "coverage": [null,1,null,null,0,null,null,1]
+              "source_digest": "c006f07c5cf67adcdb328acb8f085dd9",
+              "coverage": [null,1,null,null,0,null,null,1,null,null,null,null,0,0,null,0]
           }
     ]
   }
@@ -164,7 +164,7 @@ From CircleCI to Codecov.
   Info: using job ID variable $CIRCLE_BUILD_NUM
   Info: found coverage files in './'
   Info: Processing file 'test.ml'...
-  Info: ... file has 3 points
+  Info: ... file has 6 points
   Info: sending to Codecov with command:
   Info: curl -s https://codecov.io/bash | bash -s -- -Z -f coverage.json
   $ cat coverage.json
@@ -176,8 +176,8 @@ From CircleCI to Codecov.
     "source_files": [
           {
               "name": "test.ml",
-              "source_digest": "55ec0167abb99a3141afb092c92d0e23",
-              "coverage": [null,1,null,null,0,null,null,1]
+              "source_digest": "c006f07c5cf67adcdb328acb8f085dd9",
+              "coverage": [null,1,null,null,0,null,null,1,null,null,null,null,0,0,null,0]
           }
     ]
   }
@@ -212,7 +212,7 @@ From GitHub Actions to Coveralls.
   Info: including git info
   Info: found coverage files in './'
   Info: Processing file 'test.ml'...
-  Info: ... file has 3 points
+  Info: ... file has 6 points
   Info: sending to Coveralls with command:
   Info: curl -L -F json_file=@./coverage.json https://coveralls.io/api/v1/jobs
   $ cat coverage.json
@@ -225,8 +225,8 @@ From GitHub Actions to Coveralls.
     "source_files": [
           {
               "name": "test.ml",
-              "source_digest": "55ec0167abb99a3141afb092c92d0e23",
-              "coverage": [null,1,null,null,0,null,null,1]
+              "source_digest": "c006f07c5cf67adcdb328acb8f085dd9",
+              "coverage": [null,1,null,null,0,null,null,1,null,null,null,null,0,0,null,0]
           }
     ]
   }
@@ -241,7 +241,7 @@ From GitHub Actions to Codecov
   Info: using job ID variable $GITHUB_RUN_NUMBER
   Info: found coverage files in './'
   Info: Processing file 'test.ml'...
-  Info: ... file has 3 points
+  Info: ... file has 6 points
   Info: sending to Codecov with command:
   Info: curl -s https://codecov.io/bash | bash -s -- -Z -f coverage.json
   $ cat coverage.json
@@ -253,8 +253,8 @@ From GitHub Actions to Codecov
     "source_files": [
           {
               "name": "test.ml",
-              "source_digest": "55ec0167abb99a3141afb092c92d0e23",
-              "coverage": [null,1,null,null,0,null,null,1]
+              "source_digest": "c006f07c5cf67adcdb328acb8f085dd9",
+              "coverage": [null,1,null,null,0,null,null,1,null,null,null,null,0,0,null,0]
           }
     ]
   }

test/report/test.ml
@@ -6,3 +6,11 @@ let g () =
 
 let () =
   f ()
+
+(* Reproduces a HTML display bug that existed in development between 2.6.3 and
+   2.7.0, starting with 1b8d7ec5985aa12a85e797e3d53fc72713e80c35. *)
+let a () =
+  if true then
+    true
+  else
+    false

test/report/text.t
@@ -7,4 +7,4 @@
   $ dune exec ./test.exe --instrument-with bisect_ppx
   $ bisect-ppx-report summary --verbose
   Info: found coverage files in './'
-  Coverage: 2/3 (66.67%)
+  Coverage: 2/6 (33.33%)