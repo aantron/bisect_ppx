f5bb50cc22d550d286b9f38ce7b786a09ccc41c3
Arvid Jakobsson <arvid.jakobsson@nomadic-labs.com>
2022-02-07 23:52:45 +0100

    fixup! tmp: test report

--- report.sh
+++ report.sh
@@ -1,2 +1,6 @@
 dune exec --instrument-with bisect_ppx fancy_index/main.exe
-dune exec ./src/report/main.exe html
+dune exec ./src/report/main.exe -- html -o _coverage --ignore-missing-files
+dune exec ./src/report/main.exe -- html --tree -o _coverage_tree --ignore-missing-files
+
+echo "See file://$(pwd)/_coverage/index.html"
+echo "See file://$(pwd)/_coverage_tree/index.html"
